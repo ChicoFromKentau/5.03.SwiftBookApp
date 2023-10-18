55f5f5bca2339f7d87e883e90810761c4a9f05da
ChicoFromKentau <chingiz.alimov@gmail.com>
2023-10-18 18:55:55 +0600

    ViewControllers were added

---
+++ SwiftBookApp/CourseList/CourseDetailsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CourseDetailsViewController.swift
+//  SwiftBookApp
+//
+//  Created by Chingiz Alimov on 18.10.2023.
+//
+
+import UIKit
+
+class CourseDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
